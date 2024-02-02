1b5c1f12819eeb3b198037c7f5e66e7195c2833e
tali <iitalics@gmail.com>
2024-02-02 18:46:19 -0500

    changed font

--- emacs/custom.el
+++ emacs/custom.el
@@ -31,13 +31,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :slant normal :weight semi-bold :height 120 :width normal :foundry "CTDB" :family "Fira Code"))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#1D252C" :foreground "#A0B3C5" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 120 :width normal :foundry "CTDB" :family "Fira Code"))))
  '(ivy-minibuffer-match-face-2 ((t (:weight normal))))
  '(ivy-minibuffer-match-face-3 ((t (:weight normal))))
  '(ivy-minibuffer-match-face-4 ((t (:weight normal))))
  '(lsp-headerline-breadcrumb-path-error-face ((t (:inherit lsp-headerline-breadcrumb-path-face))))
  '(lsp-headerline-breadcrumb-path-face ((t (:inherit mode-line-face))))
  '(lsp-headerline-breadcrumb-separator-face ((t (:inherit mode-line-face :weight thin))))
+ '(merlin-compilation-error-face ((t (:underline (:color "#D95468" :style wave :position nil)))))
  '(tuareg-font-lock-error-face ((t (:underline (:color "firebrick" :style wave)))))
  '(tuareg-font-lock-governing-face ((t (:inherit font-lock-keyword-face))))
  '(tuareg-font-lock-operator-face ((t nil))))
